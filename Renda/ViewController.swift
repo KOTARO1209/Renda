a2035d514ee8624d28c3532ad64e17b862c27d9a
KOTARO1209 <kmachida@mxdlab.net>
2023-02-10 11:45:22 +0900

    Initial Commit

---
+++ Renda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Renda
+//
+//  Created by まちだこうたろう on 2023/02/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
